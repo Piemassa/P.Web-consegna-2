8b72ff464b8aa36588998c9c20cf0db658e6b646
norarica <122795434+norarica@users.noreply.github.com>
2024-12-17 00:21:15 +0100

    Add files via upload

--- pnldb.sql
+++ pnldb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Creato il: Dic 15, 2024 alle 15:47
+-- Creato il: Dic 16, 2024 alle 23:20
 -- Versione del server: 8.0.35
 -- Versione PHP: 8.2.20
 
@@ -48,16 +48,31 @@ CREATE TABLE `CONTIENE` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `CORSI`
+-- Struttura della tabella `CORSO`
 --
 
-CREATE TABLE `CORSI` (
-  `Corsi_Nome` varchar(32) DEFAULT NULL,
-  `Corso_Operatore_Nome` varchar(32) DEFAULT NULL,
-  `Corso_Operatore_Cognome` varchar(32) DEFAULT NULL,
+CREATE TABLE `CORSO` (
+  `Prodotto_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Corso_Nome` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Corso_Operatore_Nome` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Corso_Operatore_Cognome` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Corso_Data` varchar(32) DEFAULT NULL,
-  `Prodotto_id` varchar(255) NOT NULL
+  `N_Lez` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `CORSO`
+--
+
+INSERT INTO `CORSO` (`Prodotto_id`, `Corso_Nome`, `Corso_Operatore_Nome`, `Corso_Operatore_Cognome`, `Corso_Data`, `N_Lez`) VALUES
+('C1', 'Corso di Canto (15 lezioni)', 'Claudio', 'Forte', '', 15),
+('C2', 'Corso di Canto (30 lezioni)', 'Claudio', 'Forte', '', 30),
+('C3', 'Corso di Mix&Master (15 lezioni)', 'Giancarlo', 'Piro', '', 15),
+('C4', 'Corso di Mix&Master (30 lezioni)', 'Giancarlo', 'Piro', '', 30),
+('Cm1', 'Beat Making', 'Giorgio', 'Parodi', '2025-01-01', 1),
+('Cm2', 'Recording Vocals', 'Arianna', 'Vandelli', '2025-01-14', 1),
+('Cm3', 'Soundtrack Crafting', 'Ginevra', 'Morselli', '2025-02-14', 1),
+('Cm4', 'Loop & Groove', 'Daniele', 'Righi', '2025-02-26', 1);
 
 -- --------------------------------------------------------
 
@@ -107,6 +122,23 @@ CREATE TABLE `PRODOTTO` (
   `Prodotto_descrizione` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Dump dei dati per la tabella `PRODOTTO`
+--
+
+INSERT INTO `PRODOTTO` (`Prodotto_id`, `Prodotto_prezzo`, `Prodotto_immagine`, `Prodotto_descrizione`) VALUES
+('C1', 600.00, 'https://file.cure-naturali.it/site/image/hotspot_article_first/24008.jpg', '15 lezioni da un\'ora personalizzate e mirate ai tuoi obbiettivi nel canto'),
+('C2', 900.00, 'https://file.cure-naturali.it/site/image/hotspot_article_first/24008.jpg', '30 lezioni da un\'ora personalizzate e mirate ai tuoi obbiettivi nel canto'),
+('C3', 850.00, 'https://www.artandmusicstudios.com/wp-content/uploads/2019/12/Mix-Master.png', '15 lezioni personalizzate da un\'ora sull\'arte del Mix&master'),
+('C4', 1360.00, 'https://www.artandmusicstudios.com/wp-content/uploads/2019/12/Mix-Master.png', '30 lezioni personalizzare da un\'ora e sull\'arte del Mix&master'),
+('Cm1', 150.00, 'https://audient.com/wp-content/uploads/2021/07/image1.jpg', 'Le basi del beat making per i generi hip-hop, trap e EDM.'),
+('Cm2', 150.00, 'https://media.istockphoto.com/id/635926346/it/foto/apparecchiature-di-registrazione-in-studio.jpg?s=612x612&w=0&k=20&c=DeBtSR6d2i3djlJOLgG-PqzGpikFNm4PD9tlzDJo1MY=', 'Diverse tecniche per la registrazione delle voci in studio.'),
+('Cm3', 150.00, 'https://www.cinematographe.it/wp-content/uploads/2015/01/Film-Music.jpg', 'Come comporre colonne sonore per il cinema. Rapporto tra emozione, immagine e suono.'),
+('Cm4', 150.00, 'https://unison.audio/wp-content/uploads/Drum-Loops-1.png.webp', 'Come creare brani in tempo reale con loop station.'),
+('S1', 60.00, 'https://www.musiclabstudio.com/wp-content/uploads/2019/03/costo-sala-prove-1024x768.jpg', 'Questa sala offre un ambiente accogliente e versatile, perfetto per band e musicisti. Dotata di una batteria acustica di qualità e amplificatori per chitarre, è ideale per registrazioni dal vivo.'),
+('S2', 60.00, 'https://www.sammusicstudios.it/images/Sala_Vintage_Sala_Prove.jpg', 'Una sala spaziosa e moderna pensata per band e musicisti. Offre uno spazio ideale per sessioni di registrazione di gruppo, con un mix tra comfort e funzionalità tecnica.'),
+('SV1', 60.00, 'https://static.wixstatic.com/media/11062b_2996c6170ffb4b1d929439162f981ce3~mv2.jpg', 'Una volta acquistato il numero di Mix che desideri, ci metteremo in contatto con te per realizzare il tuo progetto');
+
 -- --------------------------------------------------------
 
 --
@@ -114,11 +146,19 @@ CREATE TABLE `PRODOTTO` (
 --
 
 CREATE TABLE `SALA` (
-  `Sala_Tipo` varchar(32) DEFAULT NULL,
-  `Sala_Nome` varchar(32) DEFAULT NULL,
   `Sala_capienza` int DEFAULT NULL,
-  `Prodotto_id` varchar(255) NOT NULL
+  `Prodotto_id` varchar(255) NOT NULL,
+  `Sala_Tipo` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Sala_Nome` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `SALA`
+--
+
+INSERT INTO `SALA` (`Sala_capienza`, `Prodotto_id`, `Sala_Tipo`, `Sala_Nome`) VALUES
+(5, 'S1', 'Sala registrazione', 'Lumen'),
+(7, 'S2', 'Sala registrazione', 'Arcadia');
 
 -- --------------------------------------------------------
 
@@ -127,11 +167,18 @@ CREATE TABLE `SALA` (
 --
 
 CREATE TABLE `SERVIZIO` (
-  `Servizio_Tipo` varchar(32) DEFAULT NULL,
-  `Servizio_Operatore_Nome` varchar(32) DEFAULT NULL,
   `Servizio_Operatore_Cognome` varchar(32) DEFAULT NULL,
-  `Prodotto_id` varchar(255) NOT NULL
+  `Prodotto_id` varchar(255) NOT NULL,
+  `Servizio_Tipo` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Servizio_Operatore_Nome` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `SERVIZIO`
+--
+
+INSERT INTO `SERVIZIO` (`Servizio_Operatore_Cognome`, `Prodotto_id`, `Servizio_Tipo`, `Servizio_Operatore_Nome`) VALUES
+('Lucarini', 'SV1', 'Mix', 'Pietro');
 
 -- --------------------------------------------------------
 
@@ -156,13 +203,13 @@ CREATE TABLE `UTENTE` (
 --
 
 INSERT INTO `UTENTE` (`U_cf`, `U_mail`, `U_password`, `U_nome`, `U_cognome`, `U_tipo`, `U_data_di_nascita`, `U_telefono`, `U_stato`) VALUES
-('123456789agh', 'igmiti@gmail.com', '1234', 'Igor', 'miti ', 'cliente', '1996-12-17', '254269890867', 'approvato'),
-('123456789aghjkfr', 'igor_miti@gmail.com', '1234', 'Igor', 'miti ', 'cliente', '1996-12-17', '254269890867', 'approvato'),
-('835109kjadbv', 'luca.gotto@gmai.com', '1234', 'luca', 'gotto', 'cliente', '2001-05-24', '319875', 'attivo'),
-('aodgiuab246', 'checca.catone@gmail.com', '12345', 'checca ', 'catone', 'cliente', '1978-06-23', '9382592847', 'attivo'),
-('cfadmin111111', 'admin@gmail.com', 'root', 'admin1', 'admin1', 'admin', '2000-01-01', '103458y74', 'attivo'),
-('cfadmin222222', 'admin2@gmail.com', 'root', 'admin2', 'admin2', 'admin', '2000-01-01', '1262413651', 'attivo'),
-('qioyt135316', 'proprietario@gmail.com', '1234', 'proprietario1', 'proprietario', 'proprietario', '2000-03-23', '23415134124', 'attivo');
+('36bcfuianq8', 'io@gmail.com', '1234', 'io', 'mitico', 'cliente', '0052-04-04', '456785679', 'attivo'),
+('3y98241jhhjsja', 'igor.miti@gmail.com', '1234', 'igor', 'miti', 'cliente', '0057-04-12', '4567890', 'attesa'),
+('53278hdsh', 'prop@gmail.com', 'mio', 'proprietario1', 'P1', 'proprietario', '0782-05-04', '3456788', 'attivo'),
+('dgdwjkd787289', 'admin2@gmail.com', 'root', 'admin2', 'A2', 'admin', '0723-06-30', '567890878', 'attivo'),
+('e5678nsjj', 'norarica@gmail.com', '12345', 'nora', 'rica', 'cliente', '0008-07-25', '5467890', 'attivo'),
+('sddfhjkjk', 'admin@gmail.com', 'root', 'admin1', 'A1', 'admin', '0095-04-23', '3886945278', 'attivo'),
+('wfkjnjnoicf', 'm.rossi@gmail.com', '123', 'Mario', 'Rossi', 'cliente', '0045-03-12', '3456789', 'attesa');
 
 --
 -- Indici per le tabelle scaricate
@@ -183,9 +230,9 @@ ALTER TABLE `CONTIENE`
   ADD KEY `Prodotto_id` (`Prodotto_id`);
 
 --
--- Indici per le tabelle `CORSI`
+-- Indici per le tabelle `CORSO`
 --
-ALTER TABLE `CORSI`
+ALTER TABLE `CORSO`
   ADD PRIMARY KEY (`Prodotto_id`);
 
 --
@@ -241,27 +288,27 @@ ALTER TABLE `UTENTE`
 -- Limiti per la tabella `CARRELLO`
 --
 ALTER TABLE `CARRELLO`
-  ADD CONSTRAINT `carrello_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`),
-  ADD CONSTRAINT `carrello_ibfk_2` FOREIGN KEY (`U_cf`) REFERENCES `UTENTE` (`U_cf`);
+  ADD CONSTRAINT `carrello_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `carrello_ibfk_2` FOREIGN KEY (`U_cf`) REFERENCES `UTENTE` (`U_cf`) ON DELETE CASCADE;
 
 --
 -- Limiti per la tabella `CONTIENE`
 --
 ALTER TABLE `CONTIENE`
-  ADD CONSTRAINT `contiene_ibfk_1` FOREIGN KEY (`Ordine_id`) REFERENCES `ORDINE` (`Ordine_id`),
+  ADD CONSTRAINT `contiene_ibfk_1` FOREIGN KEY (`Ordine_id`) REFERENCES `ORDINE` (`Ordine_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `contiene_ibfk_2` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`);
 
 --
--- Limiti per la tabella `CORSI`
+-- Limiti per la tabella `CORSO`
 --
-ALTER TABLE `CORSI`
-  ADD CONSTRAINT `corsi_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`);
+ALTER TABLE `CORSO`
+  ADD CONSTRAINT `corso_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`) ON DELETE CASCADE;
 
 --
 -- Limiti per la tabella `EFFETTUA`
 --
 ALTER TABLE `EFFETTUA`
-  ADD CONSTRAINT `effettua_ibfk_1` FOREIGN KEY (`Ordine_id`) REFERENCES `ORDINE` (`Ordine_id`),
+  ADD CONSTRAINT `effettua_ibfk_1` FOREIGN KEY (`Ordine_id`) REFERENCES `ORDINE` (`Ordine_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `effettua_ibfk_2` FOREIGN KEY (`U_cf`) REFERENCES `UTENTE` (`U_cf`);
 
 --
@@ -275,13 +322,13 @@ ALTER TABLE `MODIFICA`
 -- Limiti per la tabella `SALA`
 --
 ALTER TABLE `SALA`
-  ADD CONSTRAINT `sala_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`);
+  ADD CONSTRAINT `sala_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`) ON DELETE CASCADE;
 
 --
 -- Limiti per la tabella `SERVIZIO`
 --
 ALTER TABLE `SERVIZIO`
-  ADD CONSTRAINT `servizio_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`);
+  ADD CONSTRAINT `servizio_ibfk_1` FOREIGN KEY (`Prodotto_id`) REFERENCES `PRODOTTO` (`Prodotto_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
